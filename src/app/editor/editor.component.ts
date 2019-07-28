5acbecd383e997234ba4dedc8f41083a732df1b6
EvansMatthew97 <evans.matthew97@gmail.com>
2019-07-28 17:41:09 +0200

    Load hierarchy from server instead of mocking

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PanZoomConfig } from 'ng2-panzoom';
 import { Employee } from '../interfaces/employee.interface';
+import { ApiService } from '../api/api.service';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent {
+export class EditorComponent implements OnInit {
   zoomConfig: PanZoomConfig = new PanZoomConfig({
     zoomLevels: 10,
     freeMouseWheelFactor: 0.01,
@@ -15,59 +16,18 @@ export class EditorComponent {
     keepInBounds: true,
   });
 
-  hierarchy: Employee[] = [
-    {
-        "id": 1,
-        "name": "John",
-        "surname": "Smith",
-        "birthdate": new Date("1960-03-04T00:00:00.000Z"),
-        "salary": 700000,
-        "oversees": [
-            {
-                "id": 2,
-                "name": "Jane",
-                "surname": "Doe",
-                "birthdate": new Date("1980-05-06T00:00:00.000Z"),
-                "salary": 500000,
-                "oversees": [
-                    {
-                        "id": 3,
-                        "name": "Jim",
-                        "surname": "Bean",
-                        "birthdate": new Date("1978-01-03T00:00:00.000Z"),
-                        "salary": 650000,
-                        "oversees": []
-                    },
-                    {
-                        "id": 4,
-                        "name": "Roger",
-                        "surname": "Wilco",
-                        "birthdate": new Date("1995-12-11T00:00:00.000Z"),
-                        "salary": 200000,
-                        "oversees": []
-                    },
-                    {
-                        "id": 5,
-                        "name": "Susan",
-                        "surname": "Roe",
-                        "birthdate": new Date("1994-10-10T00:00:00.000Z"),
-                        "salary": 250000,
-                        "oversees": []
-                    }
-                ]
-            },
-            {
-              "id": 6,
-              "name": "Matthew",
-              "surname": "Evans",
-              "birthdate": new Date("1997-10-21T00:00:00.000Z"),
-              "salary": 250000,
-              "oversees": []
-          }
-        ]
-    }
-  ];
-  selectedId = this.hierarchy[0].id;
+  hierarchy: Employee[] = [];
+  selectedId = null;
+
+  constructor(
+    private readonly api: ApiService,
+  ) {}
+
+  async ngOnInit() {
+    this.hierarchy = await this.api.get('/employee/hierarchy', {});
+    this.selectedId = this.hierarchy[0].id || null;
+    console.log(this.hierarchy);
+  }
 
 
   selectEmployee(employee) {
--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Ng2PanZoomModule } from 'ng2-panzoom';
 import { RouterModule } from '@angular/router';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { ApiModule } from '../api/api.module';
 
 @NgModule({
   imports: [
@@ -13,6 +14,7 @@ import { EmployeeComponent } from './components/employee/employee.component';
     }]),
     CommonModule,
     Ng2PanZoomModule,
+    ApiModule,
   ],
   providers: [],
   declarations: [
